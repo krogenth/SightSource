18c52eebbca4264981bfafba9d457b2c40a4ccb4
krogenth <kelleydaniel15@gmail.com>
2021-11-02 21:20:47 -0400

    add cart and landing page components, add routing for new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: 'cart', component: CartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ LandingComponent, CartComponent ]
