6df8f1a6ef7cdd6ae1e98678ddf9d786bd4a7683
krogenth <kelleydaniel15@gmail.com>
2021-11-03 23:16:00 -0400

    add date picker to cart items, fixed some cart css issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+//  angular imports
 import { NgModule } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//  ngx-bootstrap imports
 import { AlertModule } from "ngx-bootstrap/alert";
 import { ButtonsModule } from "ngx-bootstrap/buttons";
+import { BsDatepickerModule, BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
+//  app specific imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
 import { LandingComponent } from './landing/landing.component';
-
 import { JsonService } from "./json.service";
 
 @NgModule({
@@ -26,9 +30,11 @@ import { JsonService } from "./json.service";
     BrowserAnimationsModule,
     AlertModule.forRoot(),
     ButtonsModule.forRoot(),
+    BsDatepickerModule.forRoot(),
   ],
   providers: [
     CurrencyPipe,
+    BsDatepickerConfig,
     JsonService,
   ],
   bootstrap: [
