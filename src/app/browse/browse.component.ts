bf002b6a7e2d02b1499bd8364c311715da1bf83d
Dan Klein <dklein960@gmail.com>
2021-11-30 19:56:51 -0500

    Adds filter with country selection dropdown (not currently functional)

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -17,17 +17,55 @@ import {Tour} from "../types/tour";
 export class BrowseComponent implements OnInit {
   tours: Tour[] = [];
   countries: Country[] = [];
+  items:any=[];
+  allSelected:boolean;
+  checked:any;
+  
 
   constructor(private json: JsonService) {
+    
+    this.allSelected = false;
+
     json.getData('assets/json/landing.json').subscribe(result => {
+      
       for(let i in result){
+        this.items.push(
+          {id:result[i].id,
+            value: result[i].name,
+          selected:false}
+          );
         for(let j in result[i].tours){
           this.tours.push(result[i].tours[j]);
         }
       }
+      console.log(this.items);
     });
+    this.getCheckedItems();
+
+
+  }
+  getCheckedItems(){
+    this.checked = [];
+    for (let i in this.items) {
+      if(this.items[i].selected)
+      this.checked.push(this.items[i]);
+    }
+    this.checked = JSON.stringify(this.checked);
   }
 
+  checkOrUncheckAll() {
+    for (let i in this.items) {
+      this.items[i].selected = this.allSelected;
+    }
+    this.getCheckedItems();
+  }
+
+  areAllSelected() {
+    this.allSelected = this.items.every(function(item:any) {
+        return item.selected == true;
+      })
+    this.getCheckedItems();
+  }
 
   ngOnInit(): void {
   }
